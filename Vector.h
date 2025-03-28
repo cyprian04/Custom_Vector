510dcde23d85021c43c6f2be88b94e27003d4860
Cyprian <Cyprian.Gibas@o2.pl>
2025-03-28 23:01:49 +0100

    initial commit, this is the base form of this custom container. All Vector code is placed in Vector.h file and tests are placed for now in main.cpp

---
+++ Vector.h
@@ -0,0 +1,141 @@
+#pragma once
+#include <iostream>
+#include <iterator>
+#include <cstddef>
+#include <initializer_list>
+
+
+template <typename T>
+class Vector
+{
+public:
+	explicit Vector()
+		:
+		m_size(0),
+		m_capacity(2),
+		m_buffer(new T[m_capacity])
+	{}
+	explicit Vector(const int size)
+		:
+		m_size(size),
+		m_capacity(2 * m_size),
+		m_buffer(new T[m_capacity]{ 0 })
+	{}
+	explicit Vector(const std::initializer_list<T> list)
+		:
+		m_size(0),
+		m_capacity(2 * list.size()),
+		m_buffer(new T[m_capacity]{ 0 })
+	{
+		for (auto element : list) push_back(element);
+	}
+	~Vector() { delete[] m_buffer; }
+	Vector(const Vector& src)
+		:
+		m_size(src.m_size),
+		m_capacity(src.m_capacity),
+		m_buffer(new T[m_capacity]{ 0 })
+	{
+		for (std::size_t i = 0; i < m_size; i++) m_buffer[i] = src.m_buffer[i];
+	}
+	Vector& operator=(const Vector& src) {
+
+		if (this != &src) {
+			delete[] m_buffer;
+			m_buffer = nullptr;
+
+			m_size = src.m_size;
+			m_capacity = src.m_capacity;
+			m_buffer = new T[m_capacity];
+
+			for (std::size_t i = 0; i < m_size; i++) m_buffer[i] = src.m_buffer[i];
+		}
+		return *this;
+	}
+	Vector(Vector&& src)
+		:
+		m_size(src.m_size),
+		m_capacity(src.m_capacity),
+		m_buffer(src.m_buffer)
+	{
+		src.m_size = 0;
+		src.m_capacity = 0;
+		src.m_buffer = nullptr;
+	}
+	Vector& operator=(Vector&& src) {
+		if (this != &src) {
+			delete[] m_buffer;
+			m_buffer = nullptr;
+
+			m_size = src.m_size;
+			m_capacity = src.m_capacity;
+			m_buffer = src.m_buffer;
+
+			src.m_size = 0;
+			src.m_capacity = 0;
+			src.m_buffer = nullptr;
+		}
+		return *this;
+	}
+	void push_back(const T& value) {
+		if (m_size + 1 == m_capacity) {
+			m_capacity *= 2;
+
+			T* const temp_buffer = new T[m_capacity];
+			for (size_t i = 0; i < m_size; i++)
+				temp_buffer[i] = m_buffer[i];
+
+			delete[] m_buffer;
+			m_buffer = temp_buffer;
+		}
+		m_buffer[m_size++] = value;
+	}
+	void pop() { m_buffer[m_size--] = 0; }
+	friend std::ostream& operator<<(std::ostream& os, const Vector<T>& vec) {
+		for (const auto element : vec) os << element << " ";
+		return os;
+	}
+public:
+	class Iterator
+	{
+	public:
+		explicit Iterator(T* m_ptr) : m_ptr(m_ptr) {}
+		using iterator_category = std::forward_iterator_tag;
+		using value_type = T;
+		using difference_type = std::ptrdiff_t;
+		using refernce = T&;
+		using pointer = T*;
+		bool operator==(const Iterator& rhs) const {
+			return m_ptr == rhs.m_ptr;
+		}
+		bool operator!=(const Iterator& rhs) const {
+			return m_ptr != rhs.m_ptr;
+		}
+		refernce operator*() {
+			return *m_ptr;
+		}
+		Iterator& operator++() {
+			m_ptr++;
+			return *this;
+		}
+		Iterator operator++(int) {
+			auto old_ptr = m_ptr;
+			m_ptr++;
+			return old_ptr;
+		}
+	private:
+		T* m_ptr;
+	};
+public:
+	Iterator begin() const {
+		return Iterator(&m_buffer[0]);
+	}
+	Iterator end() const {
+		return Iterator(&m_buffer[m_size]);
+	}
+private:
+	std::size_t m_size;
+	std::size_t m_capacity;
+	T* m_buffer;
+};
+
--- main.cpp
+++ main.cpp
@@ -1,7 +1,88 @@
 ﻿#include <iostream>
+#include <cstdlib>
+#include <algorithm>
+#include <vector>
+#include "Vector.h"
+
+
+void test1();
+void test2();
+void test3();
+void test4();
+void test5();
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    test1(); //ok
+    test2(); //ok
+    test3(); //ok
+    test4(); //ok
+    test5(); //ok
+    return 0;
 }
 
+
+void test1() {
+    Vector<int> vec;
+    vec.push_back(5);
+    vec.push_back(-19);
+    vec.push_back(23);
+    vec.push_back(1);
+    vec.push_back(100);
+}
+
+void test2() {
+    Vector<int> vec1;
+    vec1.push_back(5);
+    vec1.push_back(-19);
+    vec1.push_back(23);
+
+    Vector<int>vec2 = vec1;
+
+    vec2.pop();
+    vec1.push_back(999);
+}
+
+void test3() {
+    Vector<int> vec;
+    vec.push_back(120);
+    vec.push_back(-8);
+    vec.push_back(1);
+    {
+        Vector<int> vec2;
+        vec2 = vec;
+        vec2.push_back(89);
+    }
+    vec.pop();
+}
+
+void test4()
+{
+    Vector<int> vec(10);
+
+    std::generate(vec.begin(), vec.end(), []() {static int i = 0;  return i++; });
+    for (int i : vec) std::cout << i;
+
+
+    std::cout << std::endl;
+    Vector<int> vec1(15);
+    Vector<int> vec2 = std::move(vec1);
+    for (int i : vec2) std::cout << i;
+
+    Vector<int> vec3(15);
+    Vector<int> vec4(15);
+    vec4 = std::move(vec3);
+
+    for (int i : vec3) std::cout << i;
+    std::cout << std::endl;
+}
+
+void test5()
+{
+    // if we have a ctor that takes std::initializer_list as parameter then if we create obj with {} then it will invoke that ctor
+    Vector<int> v2{ 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    for (auto elemnet : v2) std::cout << elemnet << " ";
+
+    std::cout << std::endl << v2;
+}
